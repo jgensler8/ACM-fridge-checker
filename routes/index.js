2caa5c6fe4b133287d11e575b8da945db0aa6830
jgensl2 <jgensl2@people.uic.edu>
2014-04-27 03:15:44 -0500

    stuff

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,35 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-var doorEvent = require('../schemas/doorEvent');
+var doorEventSchema = require('../schemas/doorEvent');
 var mongooseOptions = require('../.mongolab_options.json');
 
+mongoose.connect('mongodb://'+mongooseOptions.username+":"
+  +mongooseOptions.userpass+"@ds033607.mongolab.com:33607/doortimes");
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET time*/
 router.route('/data')
  .get(function( req, res, next){
-   res.json({data: "something"});
+   doorEventSchema.find( function(err,data){
+	if(err) return res.status(200).json({error: true, type: 'DATABASE'});
+	else return res.status(200).json( data);
+   });
   })
+ /* POST new time */
  .post(function( req, res, next){
-   console.log(req.body);
-   res.status(200).json( {chase: "is cool"});
+   var newDoorEvent = new doorEventSchema({
+     timeOpen: req.body.startTime,
+     timeClosed: req.body.endTime
+   })
+   newDoorEvent.save( function(err){
+     if(err) res.status(200).json( {error: true, type: 'DATABASE'});
+     else res.status(200).json( {error: false, type: 'SUCCESS'});
+   });
  });
 
 
