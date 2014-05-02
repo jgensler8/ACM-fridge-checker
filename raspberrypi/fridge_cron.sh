09fd537fce5dadbfd070140f76a2a673b2c7f066
Chase Lee <clee231@uic.edu>
2014-05-02 00:06:16 -0500

    Added absolute path for hourly data file resets. Hopefully this fixes the bug.

--- raspberrypi/fridge_cron.sh
+++ raspberrypi/fridge_cron.sh
@@ -2,4 +2,4 @@
 
 #curl -X POST -H "Content-Type: application/json" --data @data https://acm.cs.uic.edu/~clee231/fridge/posttest.php
 curl -X POST -H "Content-Type: application/json" --data @/root/fridge/data-$(($(date +"%H") - 1)) http://jgensl2.people.uic.edu/fridge/data -v
-echo "[" > data-$(date +"%H")
+echo "[" > /root/fridge/data-$(date +"%H")
